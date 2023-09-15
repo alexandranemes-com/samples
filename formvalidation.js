15433b478dc3368508f7c1a7c230989bd1340ccb
alexandranemes-com <contact@alexandranemes.com>
2023-09-15 15:50:59 -0400

    Completed front end validation

--- formvalidation.js
+++ formvalidation.js
@@ -10,80 +10,88 @@ function validateContactForm(){
         cf_submit = document.getElementById("submit"),
         cf_project_status = document.querySelectorAll("input[name=project-type]");
     
-    const text_fields = [cf_name, cf_email, cf_org, cf_title, cf_project_details, cf_project_timeline];
+    const text_fields = [cf_name, cf_email, cf_org, cf_project_details, cf_project_timeline];
     const form_error_message = "The form contains errors or is missing information.";
     const missing_information_warning_text = "This information is required.";
     const email_missing_sign = "Email must contain '@' sign.";
+    const title_error = "You must select an option.";
 
-
-
-    // validate EACH field onblur
+    // validate EACH text field onblur
     text_fields.forEach((item) => {
         item.addEventListener("blur", (e) => {
             e.preventDefault();
             if(item.value.trim() == ""){
-                create_warning_dom(item);
+                create_warning_dom(item, missing_information_warning_text);
             }
         })
     });
 
-    // remove warning label when user returns to EACH field
+    // remove warning label when user returns to EACH text field
     text_fields.forEach((item) => {
-        item.addEventListener("focus", (e) => {
+        item.addEventListener("input", (e) => {
             e.preventDefault();
-            if(item.value.trim() == ""){
-                remove_warning_dom(item);
-            }
+            remove_warning_dom(item.nextElementSibling);
         })
     })
     
     // TODO validate entire form on submit
     cf.addEventListener("submit", (e) => {
         e.preventDefault();
-
-        // validate radio buttons
-        let a_radio_button_was_checked = false;
-        cf_project_status.forEach((item) => {
-            if(item.checked == true){
-                a_radio_button_was_checked = true;
+        
+        // validate text fields
+        text_fields.forEach((item) => {
+            if(item.value.length == 0){
+                create_warning_dom(item, missing_information_warning_text);
             } else {
-                create_warning_dom(item, missing_information_warning_text)
+                remove_warning_dom(item.nextElementSibling);
             }
         });
 
-        //validate email address
-        let email_format_ok = false;
-        if(cf_email.value.includes("@") == false){
-            create_warning_dom(cf_email, email_missing_sign);
+        //validate dropdown (title)
+        let cf_title_picked = false;
+        if(cf_title.value === ""){
+            create_warning_dom(cf_title, title_error)
         } else {
-            email_format_ok = true;
+            cf_title_picked = true;
+            remove_warning_dom(cf_title.nextElementSibling);
         }
 
-        if(a_radio_button_was_checked == true && email_format_ok == true){
+        // validate radio buttons
+        let project_stage_area = document.getElementById("project-stage");
+        let a_radio_button_was_checked = false;
+        create_warning_dom(project_stage_area, missing_information_warning_text);
+        cf_project_status.forEach((item) => {
+            if(item.checked){
+                a_radio_button_was_checked = true;
+                remove_warning_dom(project_stage_area.nextElementSibling);
+            }
+        });
+
+        if(a_radio_button_was_checked == true && cf_title_picked == true){
             cf_validated == true;
+            remove_warning_dom(cf.nextElementSibling);
         } else {
-            create_warning_dom(cf, form_error_message)
+            create_warning_dom(cf, form_error_message);
         }
     });
-
 }
 
 // build DOM warning
 function create_warning_dom (element, message){
-    // const warning_text = "This information is required.";
-    let warning_label = document.createElement("label");
+    let warning_label = element.nextElementSibling;
 
-    element.insertAdjacentElement("afterend", warning_label);
-    warning_label.classList.add('warning-label');
-    warning_label.textContent = message;
+    if(warning_label == null || warning_label.matches(".warning-label") == false){
+        warning_label = document.createElement("label");
+        element.insertAdjacentElement("afterend", warning_label);
+        warning_label.classList.add('warning-label');
+        warning_label.textContent = message;
+    }
 }
 
 // remove DOM warning
 function remove_warning_dom (element){
-    let warning_label = element.nextSibling;
-    
-    if(warning_label != null){
-        warning_label.remove();
+    if(element != null && element.matches(".warning-label")){
+        element.remove();
     }
 }
 
